33ae3b71c1bb44a11e3440577968e70f442c2258
Dibyanshoo Pandey <76901080+Blackdevil2018@users.noreply.github.com>
2021-10-15 08:21:15 +0530

    Update MicroEdit.py

--- MicroEdit.py
+++ MicroEdit.py
@@ -2,7 +2,6 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
-# Defining TextEditor Class
 class TextEditor:
   def __init__(self,root):
     self.root = root
@@ -16,7 +15,6 @@ class TextEditor:
     self.settitle()
     self.statusbar = Label(self.root,textvariable=self.status,font=("times new roman",15,"bold"),bd=2,relief=GROOVE)
     self.statusbar.pack(side=BOTTOM,fill=BOTH)
-    # Initializing Status
     self.status.set("Welcome To Text Editor")
     self.menubar = Menu(self.root,font=("times new roman",15,"bold"),activebackground="skyblue")
     self.root.config(menu=self.menubar)
@@ -32,93 +30,56 @@ class TextEditor:
     self.editmenu.add_command(label="Cut",accelerator="Ctrl+X",command=self.cut)
     self.editmenu.add_command(label="Copy",accelerator="Ctrl+C",command=self.copy)
     self.editmenu.add_command(label="Paste",accelerator="Ctrl+V",command=self.paste)
-    # Adding Seprator
     self.editmenu.add_separator()
-    # Adding Undo text Command
     self.editmenu.add_command(label="Undo",accelerator="Ctrl+U",command=self.undo)
-    # Cascading editmenu to menubar
     self.menubar.add_cascade(label="Edit", menu=self.editmenu)
-    # Creating Help Menu
     self.helpmenu = Menu(self.menubar,font=("times new roman",12,"bold"),activebackground="skyblue",tearoff=0)
-    # Adding About Command
     self.helpmenu.add_command(label="About",command=self.infoabout)
-    # Cascading helpmenu to menubar
     self.menubar.add_cascade(label="Help", menu=self.helpmenu)
-    # Creating Scrollbar
     scrol_y = Scrollbar(self.root,orient=VERTICAL)
-    # Creating Text Area
     self.txtarea = Text(self.root,yscrollcommand=scrol_y.set,font=("times new roman",15,"bold"),state="normal",relief=GROOVE)
-    # Packing scrollbar to root window
     scrol_y.pack(side=RIGHT,fill=Y)
-    # Adding Scrollbar to text area
     scrol_y.config(command=self.txtarea.yview)
-    # Packing Text Area to root window
     self.txtarea.pack(fill=BOTH,expand=1)
-    # Calling shortcuts funtion
     self.shortcuts()
-  # Defining settitle function
   def settitle(self):
     if self.filename:
-      # Updating Title as filename
       self.title.set(self.filename)
     else:
       # Updating Title as Untitled
       self.title.set("Untitled")
-  # Defining New file Function
   def newfile(self,*args):
-    # Clearing the Text Area
     self.txtarea.delete("1.0",END)
-    # Updating filename as None
     self.filename = None
-    # Calling settitle funtion
     self.settitle()
-    # updating status
     self.status.set("New File Created")
-  # Defining Open File Funtion
   def openfile(self,*args):
-    # Exception handling
     try:
-      # Asking for file to open
       self.filename = filedialog.askopenfilename(title = "Select file",filetypes = (("All Files","*.*"),("Text Files","*.txt"),("Python Files","*.py")))
-      # checking if filename not none
       if self.filename:
-        # opening file in readmode
         infile = open(self.filename,"r")
-        # Clearing text area
         self.txtarea.delete("1.0",END)
-        # Inserting data Line by line into text area
         for line in infile:
           self.txtarea.insert(END,line)
-        # Closing the file  
         infile.close()
-        # Calling Set title
         self.settitle()
-        # Updating Status
         self.status.set("Opened Successfully")
     except Exception as e:
       messagebox.showerror("Exception",e)
-  # Defining Save File Funtion
   def savefile(self,*args):
-    # Exception handling
     try:
       if self.filename:
         data = self.txtarea.get("1.0",END)
         outfile = open(self.filename,"w")
-        # Writing Data into file
         outfile.write(data)
-        # Closing File
         outfile.close()
-        # Calling Set title
         self.settitle()
-        # Updating Status
         self.status.set("Saved Successfully")
       else:
         self.saveasfile()
     except Exception as e:
       messagebox.showerror("Exception",e)
-  # Defining Save As File Funtion
   def saveasfile(self,*args):
-    # Exception handling
     try:
    
       untitledfile = filedialog.asksaveasfilename(title = "Save file As",defaultextension=".txt",initialfile = "Untitled.txt",filetypes = (("All Files","*.*"),("Text Files","*.txt"),("Python Files","*.py")))
@@ -173,6 +134,8 @@ class TextEditor:
     self.txtarea.bind("<Control-c>",self.copy)
     self.txtarea.bind("<Control-v>",self.paste)
     self.txtarea.bind("<Control-u>",self.undo)
+    self.textarea.bind("<Control-u>",self.delete)
+    self.textarea.bind(<"Control-z>",self.split)
 root = Tk()
 TextEditor(root)
 root.mainloop()
